54961b1026fd7c9bbd3b3b726740f5f8c883f6ab
Diego García <a20172367@pucp.edu.pe>
2021-04-20 01:02:17 -0500

    controller de ActividadesController

--- src/main/java/com/ipt/dashboard/controller/ActividadesController.java
+++ src/main/java/com/ipt/dashboard/controller/ActividadesController.java
@@ -1,5 +1,7 @@
 package com.ipt.dashboard.controller;
 
+import com.ipt.dashboard.entity.Actividades;
+import com.ipt.dashboard.entity.Proyecto;
 import com.ipt.dashboard.repository.ActividadesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/actividades")
 public class ActividadesController {
@@ -16,22 +20,28 @@ public class ActividadesController {
     @Autowired
     ActividadesRepository actividadesRepository;
 
-    @GetMapping("/nuevo")
-    public String crearActividad(){
-        return "";
+   // @GetMapping("/nuevo")
+    //public String crearActividad(){
+       // return "";
     }
-    @GetMapping("/editar")
-    public String crearActividad(){
-        return "";
-    }
-    @PostMapping("/guardar")
-    public String crearActividad(){
-        return "";
-    }
+    //@GetMapping("/editar")
+    //public String crearActividad(){
+      //  return "";
+    //}
+    //@PostMapping("/guardar")
+    //public String crearActividad(){
+      //  return "";
+    //}
+
     @GetMapping("/borrar")
     public String crearActividad(@RequestParam("id") int id,
                                  RedirectAttributes attr){
-        return "";
+        Optional<Actividades> actividadesOptional = actividadesRepository.findById(id);
+        if(actividadesOptional.isPresent()){
+            actividadesRepository.deleteById(id);
+            attr.addFlashAttribute("msgDelete","Actividad borrada exitosamente");
+        }
+        return "redirect:/proyecto/edit";
     }
 
 }
