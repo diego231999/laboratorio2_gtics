a63addeb29ac2a0aa1febb6ace311d406e9ec8b3
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2021-04-19 22:13:00 -0500

    + Se completo la actividad 2 (CRUD de flujo Area) + Se añadio los flash attributes requeridos

--- src/main/java/com/ipt/dashboard/repository/UsuarioRepository.java
+++ src/main/java/com/ipt/dashboard/repository/UsuarioRepository.java
@@ -3,5 +3,10 @@ package com.ipt.dashboard.repository;
 import com.ipt.dashboard.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+    List<Usuario> findAllByIdarea(int areaid);
+
 }
